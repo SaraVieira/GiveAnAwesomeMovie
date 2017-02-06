417289de02c19d322e55f8fbf29a516349655e3c
Sara Vieira <sara.vieira@mindera.com>
2017-02-06 23:52:10 +0000

    add logo

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app" class="columns">
+    <nav>
+      <img src="./assets/logo.svg" />
+    </nav>
     <hello></hello>
   </div>
 </template>
@@ -23,5 +26,21 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   background: #FBFEF9;
+}
+
+nav {
+  width: 100px;
+  height: 140px;
+  background: #FBFEF9;
+  position: fixed;
+  z-index: 99;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    max-height: 70%;
+    max-width: 70%;
+  }
 }
 </style>
