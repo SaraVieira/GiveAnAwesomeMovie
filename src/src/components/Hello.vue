ad9df8f27d69912160c33175d5b8cea73685f85e
Sara Vieira <sara.vieira@mindera.com>
2017-02-06 23:40:03 +0000

    add movie-details

--- src/src/components/Hello.vue
+++ src/src/components/Hello.vue
@@ -1,25 +1,45 @@
 <template>
-  <div v-if="loaded" class="column is-12">
-    <section class="is-12 column backdrop-wrapper">
+  <div v-if="loaded" class="wrapper">
+  <div class="column is-12 main m-t-0">
+    <section class="is-12 column backdrop-wrapper p-a-0">
       <img class="backdrop" v-bind:src="'https://image.tmdb.org/t/p/w1000/' + movie.backdrop_path"/>
     </section>
+    <div class="divider"></diV>
 
     <div class="column is-2 is-offset-1 poster">
-            <img v-bind:src="'https://image.tmdb.org/t/p/w300/' + movie.poster_path">
-         <h1 class="title">{{ movie.original_title }}</h1>
+      <img v-bind:src="'https://image.tmdb.org/t/p/w300/' + movie.poster_path">
+      <star-rating v-bind:increment="0.5"
+               v-bind:max-rating="5"
+               v-bind:rating="movie.vote_average / 2"
+               read-only="true"
+               inactive-color="#000004"
+               active-color="#0C6291"
+               :star-size="25"
+               class="justify-center"
+               text-class="rating">
+     </star-rating>
     </div>
+    <div class="column is-6 is-offset-1 details">
+      <h1 class="title">{{ movie.original_title }}</h1>
+  </div>
+  </div>
+  <movie-details v-bind:movie="movie"></movie-details>
   </div>
   <Loading v-else></Loading>
 </template>
 
 <script>
 import Loading from './loading'
+import StarRating from 'vue-star-rating'
+import movieDetails from './movie-details'
 
 /* global fetch */
 export default {
   name: 'hello',
   components: {
-    Loading
+    Loading,
+    StarRating,
+    movieDetails
   },
   mounted: function () {
     this.getMovies()
@@ -63,15 +83,57 @@ export default {
   }
 }
 
+.details {
+  margin-top: -250px;
+  z-index: 2;
+  position: relative;
+
+  h1 {
+    font-weight: 800;
+    font-size: 36px;
+    color: #FBFEF9;
+    text-align: left;
+  }
+}
+
+.rating {
+  color: #FBFEF9;
+}
+
 .poster {
   position: absolute;
   transform: translateY(-50%);
   left: 0;
 
   img {
-    border: 5px solid #eaeaea;
+    border: 5px solid #FBFEF9;
     box-shadow: 1px 1px 3px rgba(0,0,0,0.2);
   }
 }
 
+.divider {
+  height: 60px;
+  background: #A63446;
+  width: 100%;
+  transform: translateY(-50%);
+}
+
+
+.wrapper {
+  width: 100%;
+
+  .main {
+    height: 500px;
+    margin-top: 0;
+  }
+  .p-a-0 {
+    padding: 0;
+  }
+
+  .justify-center {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
 </style>
---
+++ src/src/components/movie-details.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="column is-8 is-offset-3 movie-details">
+    <b>Genres: </b>
+    <ul>
+      <li v-for="genre in movie.genres">{{genre.name}}</li>
+    </ul>
+    <div>
+      <b>Runtime: </b> {{movie.runtime}} m
+    </div>
+    <div>
+      <b>Release Date: </b> {{movie.release_date}}
+    </diV>
+    <div>
+      <b>Produced by: </b>
+      <ul>
+        <li v-for="company in movie.production_companies">{{company.name}}</li>
+      </ul>
+    </diV>
+    <h2>{{movie.title}}</h2>
+    <h3>{{movie.tagline}}</h3>
+    <b>Overview: </b>
+    <p>{{movie.overview}}</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'movieDetails',
+  components: {},
+  props: ['movie'],
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less">
+.movie-details {
+  margin-top: 60px;
+}
+
+b {
+  float: left;
+  padding-right: 5px;
+}
+
+ul {
+  li {
+    display: inline-block;
+
+    &:not(:last-child):after {
+      content: ',';
+      padding-right: 3px;
+    }
+  }
+}
+
+h2 {
+  margin-top: 10px;
+  font-weight: bold;
+  font-size: 16px;
+}
+
+h3 {
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+</style>
